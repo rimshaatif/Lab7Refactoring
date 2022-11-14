5c3cdaa7896c0a4ee0dc0dd0502e06f3c7f32602
rimshaatif <rimshaatif@cmail.carleton.ca>
2022-11-14 15:57:48 -0500

    made the class abstract

--- src/Site.java
+++ src/Site.java
@@ -1,4 +1,4 @@
-public class Site {
+public abstract class Site {
     protected static final double TAX_RATE = 0.13;
     private double baseAmount;
     private double taxAmount;
@@ -13,21 +13,10 @@ public class Site {
         return getBaseAmount() + getTaxAmount();
     }
 
-    public double getBaseAmount() {
-        return baseAmount;
-    }
+    abstract double getBaseAmount() ;
 
-    public double getTaxAmount() {
-        return taxAmount;
-    }
+    abstract double getTaxAmount() ;
 
 
-    public static void main(String[] args) {
-        LifelineSite ls = new LifelineSite();
-        ResidentialSite rs = new ResidentialSite();
-        double lsAmount = ls.getBillableAmount(10, 5);
-        double rsAmount = rs.getBillableAmount(10, 5);
 
-        System.out.println("LifeLineSite Amount: "+ lsAmount + "\n" + "ResidentialSite Amount: " + rsAmount );
-    }
 }
